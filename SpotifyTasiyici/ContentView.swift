70f79ef1cd7776fe411df55e3fc3a45dd14873d2
zaferavci1 <zaferavci89@gmail.com>
2025-10-14 20:07:12 +0300

    Initial Commit

---
+++ SpotifyTasiyici/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyTasiyici
+//
+//  Created by Zafer Avcı on 14.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyTasiyici/SpotifyTasiyiciApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyTasiyiciApp.swift
+//  SpotifyTasiyici
+//
+//  Created by Zafer Avcı on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyTasiyiciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
